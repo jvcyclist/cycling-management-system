0bc13dc0510cfe7590731979cf2a704b814dea0b
Patryk Karaś <patryk1karas@gmail.com>
2021-10-26 20:34:41 +0200

    Initial Spring-Boot project

---
+++ src/test/java/pl/karas/cyclingmanagementsystem/CyclingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karas.cyclingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
